9ab65a087169d2e2db73508de0d7f54d897bd60e
sindhueswaran <54274176+sindhueswaran@users.noreply.github.com>
2023-07-19 11:27:40 +0530

    Add files via upload

--- components/ui/button.js
+++ components/ui/button.js
@@ -1,7 +1,7 @@
 export default function Button({ children, onClick }) {
   return (
     <button
-      className="m-2 px-8 py-4 bg-fgprimary text-white rounded inline-block mt-8 font-semibold 
+      className="m-2 px-8 py-4 bg-fgprimaryfont text-white rounded inline-block mt-8 font-semibold 
       hover:shadow-md hover:bg-amber-500 active:bg-amber-800 active:shadow-inner
       ease-in-out duration-500 "
       onClick={onClick}>
